bd969771995a3f2f0db28af360a0068a7d0949aa
onthemod <onthemod@yandex.ru>
2022-10-24 01:03:00 +0300

    tests correction

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,6 +1,6 @@
-from scripts.gendiff import get_diffs_of_dicts
+from gendiff.scripts.gendiff import get_diffs_of_dicts
 
-def test_of_adding():
-    dict1 = {}
-    dict2 = {'1':'2'}
-    print(get_diffs_of_dicts)
+def test1():
+    dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    dict2 = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
