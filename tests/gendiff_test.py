2f73f9456021230fe910f7eb54d84562e1eccd31
onthemod <onthemod@yandex.ru>
2022-10-24 01:10:06 +0300

    test correctness checking

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -3,4 +3,4 @@ from gendiff.scripts.gendiff import get_diffs_of_dicts
 def test1():
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
     dict2 = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
-    assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n   - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
