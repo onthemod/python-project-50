73d5367beee27cb2a85b75c813a4fb80e11e91ea
onthemod <onthemod@yandex.ru>
2022-10-24 00:14:38 +0300

    codestyle edit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,8 +4,8 @@ import json
 
 
 def sort_func(element):
-    dict = {' ':'0', '-':'1', '+':'2'}
-    res = element[4:element.index(':')]+dict[element[2]]
+    dict = {' ': '0', '-': '1', '+': '2'}
+    res = element[4:element.index(':')] + dict[element[2]]
     return res
 
 
@@ -29,7 +29,7 @@ def get_diffs_of_dicts(file1_dict, file2_dict):
     for key in key_set2:
         result.append(f'  + {key}: {file2_dict[key]}')
 
-    result.sort(key = sort_func)
+    result.sort(key=sort_func)
     result.insert(0, '{')
     result.append('}')
     return '\n'.join(result)
@@ -39,11 +39,13 @@ def get_line_to_append(key, file1_dict, file2_dict):
     if file1_dict[key] == file2_dict[key]:
         return [f'    {key}: {file1_dict[key]}']
     else:
-        return [f'  - {key}: {file1_dict[key]}', f'  + {key}: {file2_dict[key]}']
+        return [f'  - {key}: {file1_dict[key]}',
+                f'  + {key}: {file2_dict[key]}']
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration'
+                                     ' files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', type=str, help='set format of output')
