b294fe86710c0ba99dee77ab0ba8d5c0b4fcc98d
onthemod <onthemod@yandex.ru>
2022-11-09 18:51:14 +0300

    tests added

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,6 +1,17 @@
 from gendiff.scripts.gendiff import get_diffs_of_dicts
+from gendiff.scripts.gendiff import get_diffs_of_dicts
 
 def test1():
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
     dict2 = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
     assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+
+def test2():
+    dict2 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    dict1 = {}
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  + follow: False\n  + host: hexlet.io\n  + proxy: 123.234.53.22\n  + timeout: 50\n}'
+    
+def test3():
+    dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    dict2 = {}
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: False\n  - host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n}'
