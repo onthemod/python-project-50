f4bde8bc39568f81e7acf200008fb2737b110d03
onthemod <onthemod@yandex.ru>
2022-11-18 23:46:08 +0300

    hierarchical files support added

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,6 @@ import yaml
 def generate_diff(file_path1, file_path2):
     file1_dict = get_dictionary_from_file(file_path1)
     file2_dict = get_dictionary_from_file(file_path2)
-    print(file1_dict)
     return get_diffs_of_dicts(file1_dict, file2_dict)
 
 
@@ -17,6 +16,16 @@ def get_dictionary_from_file(file_path):
         return json.load(o)
     if file_path[-4:] == 'yaml' or file_path[-3:] == 'yml':
         return yaml.safe_load(o)
+        
+        
+def cut_null_values(dictionary):
+    result = {}
+    for k,v in dictionary.items():
+        if v is None:
+            print("isNone")
+            v = 'null'
+        result[k] = v
+    return result
 
 
 def get_keys(dict1, dict2):
@@ -28,40 +37,82 @@ def get_keys(dict1, dict2):
     return keylist
 
 
-def get_diff_for_key(key, value1, value2):
+def get_diff_for_key(key, value1, value2, inset):
     if value1 is None:
-        return [wrap_added_pair(key, value2)]
+        return [wrap_added_pair(key, value2, inset)]
     if value2 is None:
-        return [wrap_removed_pair(key, value1)]
+        return [wrap_removed_pair(key, value1, inset)]
     if value1 == value2:
-        return [wrap_not_changed_pair(key, value1)]
-    return [wrap_removed_pair(key, value1), wrap_added_pair(key, value2)]
+        return [wrap_not_changed_pair(key, value1, inset)]
+    if isinstance(value1, dict) and isinstance(value2, dict):
+        return [wrap_not_changed_pair(key, get_diffs_of_dicts_list(value1, value2, inset + 1), inset)]
+    return [wrap_removed_pair(key, value1, inset), wrap_added_pair(key, value2, inset)]
 
 
-def wrap_added_pair(key, value):
-    return f'  + {key}: {value}'
+def added_pair_key_prefix(key):
+    return f'  + {key}: '
+    
+    
+def removed_pair_key_prefix(key):
+    return f'  - {key}: '
+    
+    
+def not_changed_pair_key_prefix(key):
+    return f'    {key}: '
+    
+
+def suffix(value, inset=0):
+    if isinstance(value, list):
+        return '\n'.join(value)
+    if isinstance(value, dict):
+        return get_dictionary_string(value, inset+1);
+    if isinstance(value, bool):
+        return str(value).lower()
+    return str(value)
 
 
-def wrap_removed_pair(key, value):
-    return f'  - {key}: {value}'
+def wrap_added_pair(key, value, inset):
+    return ' ' * 4 * inset + added_pair_key_prefix(key) + suffix(value, inset)
+    
+    
+def wrap_removed_pair(key, value, inset):
+    return ' ' * 4 * inset + removed_pair_key_prefix(key) + suffix(value, inset)
+    
+    
+def wrap_not_changed_pair(key, value, inset):
+    return ' ' * 4 * inset + not_changed_pair_key_prefix(key) + suffix(value, inset)
 
 
-def wrap_not_changed_pair(key, value):
-    return f'    {key}: {value}'
+def get_dictionary_string(d, inset):
+    stringlist = ['{']
+    for key, value in d.items():
+        stringlist.append( wrap_not_changed_pair(key, value, inset))
+    stringlist.append(inset * 4 * ' ' + '}')
+    return '\n'.join(stringlist)
 
 
-def get_diffs_of_dicts_list(dict1, dict2):
+def get_string_of_value(value):
+    if instanceof(value, dict):
+        res = "{"
+        for k, v in value.items():
+            res=res+'\n'+ '    '+k+': '+v
+        res = res + '\n}'
+
+
+def get_diffs_of_dicts_list(dict1, dict2, inset=0):
+    dict1 = cut_null_values(dict1)
+    dict2 = cut_null_values(dict2)
     result = []
     for key in get_keys(dict1, dict2):
-        diff_strings = get_diff_for_key(key, dict1.get(key), dict2.get(key))
+        diff_strings = get_diff_for_key(key, dict1.get(key), dict2.get(key), inset)
         result.extend(diff_strings)
+    result.insert(0, '{')
+    result.append(inset * 4 * ' ' + '}') 
     return result
 
 
 def get_diffs_of_dicts(dict1, dict2):
     diff_list = get_diffs_of_dicts_list(dict1, dict2)
-    diff_list.insert(0, '{')
-    diff_list.append('}')
     return '\n'.join(diff_list)
 
 
--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,16 +1,26 @@
 from gendiff.scripts.gendiff import get_diffs_of_dicts
 from gendiff.scripts.gendiff import get_dictionary_from_file
 from gendiff.scripts.gendiff import generate_diff
+from gendiff.scripts.gendiff import get_dictionary_string
 import pytest
 
 @pytest.fixture
 def diff1():
-    return '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+    return '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
     
 
 @pytest.fixture
 def dictionary1():
     return {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
+    
+@pytest.fixture
+def dict_string_inset_1():
+    return "{\n        host: hexlet.io\n        timeout: 50\n        proxy: 123.234.53.22\n        follow: false\n    }"
+    
+def test_dict_string(dictionary1,dict_string_inset_1):
+    print(dict_string_inset_1)
+    print(get_dictionary_string({'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}, 1))
+    assert get_dictionary_string({'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}, 1) == dict_string_inset_1
 
 def test1(diff1):
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
@@ -20,13 +30,20 @@ def test1(diff1):
 def test2():
     dict2 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
     dict1 = {}
-    assert get_diffs_of_dicts(dict1, dict2)=='{\n  + follow: False\n  + host: hexlet.io\n  + proxy: 123.234.53.22\n  + timeout: 50\n}'
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  + follow: false\n  + host: hexlet.io\n  + proxy: 123.234.53.22\n  + timeout: 50\n}'
     
 def test3():
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
     dict2 = {}
-    assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: False\n  - host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n}'
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  - follow: false\n  - host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n}'
     
+    
+def test_dict_appeared():
+    dict1 = {}
+    dict2 = {'key1': {'timeout': 50, 'proxy': '123.234.53.22'}, 'key2': 'value'}
+    assert get_diffs_of_dicts(dict1, dict2)=='{\n  + key1: {\n        timeout: 50\n        proxy: 123.234.53.22\n    }\n  + key2: value\n}'
+
+
 def test4():
     file1_dict = get_dictionary_from_file('tests/file1.json')
     assert file1_dict=={'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
@@ -43,5 +60,12 @@ def test_yml(dictionary1):
     
 
 def test5():
-    diff = '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+    diff = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
     assert generate_diff('tests/file1.json','tests/file2.json')==diff
+    
+def test_nested():
+    f = open('tests/result.txt', 'r')
+    diff_strig = generate_diff('tests/nested1.json','tests/nested2.json')
+    d = zip(diff_strig.split('\n'), f)
+    for k,v in d:
+        assert k == v or k + '\n' == v
