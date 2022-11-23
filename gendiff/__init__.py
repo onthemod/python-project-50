4fa31eeb6934993c1a148efdca11ef4b72885b4d
onthemod <onthemod@yandex.ru>
2022-11-23 23:17:14 +0300

    init

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,8 +1,7 @@
 from gendiff.scripts.gendiff import generate_diff
-from gendiff.formaters import stylish
-from gendiff.plain import plain
-from gendiff.json import jsonin
+from gendiff import formaters
 
 __all__ = (
     'generate_diff',
+    'formaters',
 )
