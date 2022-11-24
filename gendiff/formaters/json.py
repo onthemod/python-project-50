32233f179da7b3a0639fe0daf0949027a244f90a
onthemod <onthemod@yandex.ru>
2022-11-24 23:34:26 +0300

    json formatter printing a value

--- gendiff/formaters/json.py
+++ gendiff/formaters/json.py
@@ -2,5 +2,4 @@ from gendiff.treenode import is_updated
 import json
 
 def stringify(lst):
-    with open("data_file.json", "w") as write_file:
-        json.dump(lst, write_file)
+    return json.dumps(lst, indent=4)
